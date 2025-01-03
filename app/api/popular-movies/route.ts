17d5c75baa9b4e7763dfb4a4c4c8b1cfed27b0e0
padmanathan10 <padmanathandev@gmail.com>
2025-01-03 12:02:15 +0530

    pagnation api fix

--- app/api/popular-movies/route.ts
+++ app/api/popular-movies/route.ts
@@ -1,17 +1,16 @@
 
-import { NextResponse } from "next/server";
-
-export async function GET() {
-
-  const apiCall = await fetch("https://api.themoviedb.org/3/movie/popular?language=en-US&page=1", {
+import { NextResponse, NextRequest } from "next/server";
+export async function GET(req:NextRequest) {
+  const searchParam =new URL(req.url)
+  const pageNumber = searchParam.searchParams.get("page") || 1;
+ 
+  const apiCall = await fetch(`https://api.themoviedb.org/3/movie/popular?language=en-US&page=${pageNumber}`, {
     method: "GET",
       headers: {
         accept: "application/json",
         Authorization: `Bearer ${process.env.TOKEN}`
       },
   })
-
   const data = await apiCall.json()
-
   return NextResponse.json(data)
 }
